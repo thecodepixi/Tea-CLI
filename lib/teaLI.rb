cdf847cd32be27cd060a89d4d6ce1d52e43f0c6f
Emily <emily.a.harber@gmail.com>
2019-10-27 14:17:15 -0400

    add display_categories method to teaLI

--- lib/teaLI.rb
+++ lib/teaLI.rb
@@ -7,14 +7,17 @@ class TeaLI
     def scrape_for_tea  
         puts "Please wait while we gather all the tea info!"
         scraper = Scraper.new 
-        scraper.get_tea_info
+        scraper.get_teas
     end 
 
-    def start_program
-
-    end 
+    def display_categories
+        Category.all.each_with_index do |category, index|
+            puts "#{index+1}. #{category.name}"
+        end 
+    end     
 
 end 
 
 tea_CLI = TeaLI.new 
 tea_CLI.scrape_for_tea 
+tea_CLI.display_categories
